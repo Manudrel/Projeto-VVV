e6677c28063b7481cae464777f07681c9312e77c
Luiz Antonio <luizantonio2mil5@gmail.com>
2026-06-15 20:21:43 -0300

    fix: fixing consultar reservas pages route

--- demo/src/main/java/com/projectvvv/domain/controller/ReservaViewController.java
+++ demo/src/main/java/com/projectvvv/domain/controller/ReservaViewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/reservas")
 public class ReservaViewController {
 
-    @GetMapping
+    @GetMapping("/consultar")
     public String consultarReservas() {
         return "reservas/consultar";
     }
