dca6c0e41baf6914b9f0c4ebc9fa06aeff65d52b
Luiz Antonio <luizantonio2mil5@gmail.com>
2026-06-15 21:39:48 -0300

    feat: creating funcionario pages

---
+++ demo/src/main/java/com/projectvvv/domain/controller/FuncionarioViewController.java
@@ -0,0 +1,37 @@
+package com.projectvvv.domain.controller;
+
+import com.projectvvv.domain.service.FuncionarioService;
+import com.projectvvv.domain.model.Funcionario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+class UsuarioLogadoMock {
+    public String getNome() { return "Marcella"; }
+    public String getCargo() { return "Gerente"; } 
+}
+
+@Controller
+@RequestMapping("/gerente/funcionarios")
+public class FuncionarioViewController {
+
+    private final FuncionarioService funcionarioService;
+
+    public FuncionarioViewController(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
+
+    @GetMapping
+    public String listarFuncionarios(Model model) {
+        model.addAttribute("usuarioLogado", new UsuarioLogadoMock());
+
+        List<Funcionario> listaFuncionarios = funcionarioService.listarTodos();
+
+        model.addAttribute("funcionarios", listaFuncionarios);
+
+        return "funcionarios/listar";
+    }
+}
