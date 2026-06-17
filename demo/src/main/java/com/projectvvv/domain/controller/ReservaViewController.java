e7918eb550c90767e497c80aeca8d0b4699c32f3
Marcella <marcella.oliveira.diasss@gmail.com>
2026-06-17 08:06:01 -0300

    fix: altera layout de Nova reserva e ajusta cidades nos seletores

--- demo/src/main/java/com/projectvvv/domain/controller/ReservaViewController.java
+++ demo/src/main/java/com/projectvvv/domain/controller/ReservaViewController.java
@@ -1,6 +1,9 @@
 package com.projectvvv.domain.controller;
 
+import com.projectvvv.domain.service.CidadeService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,13 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/reservas")
 public class ReservaViewController {
 
+    private final CidadeService cidadeService;
+
+    public ReservaViewController(CidadeService cidadeService) {
+        this.cidadeService = cidadeService;
+    }
+
     @GetMapping("/consultar")
     public String consultarReservas() {
         return "reservas/consultar";
     }
 
     @GetMapping("/nova")
-    public String novaReserva() {
+    public String novaReserva(Model model) {
+        model.addAttribute("cidades", cidadeService.listarTodas());
         return "reservas/nova";
     }
 
